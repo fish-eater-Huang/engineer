c6c02850dd2d25b58b17eeebc571158edeb5cc52
Huang <1579515851@qq.com>
2024-01-21 09:38:13 +0800

    添加了auto_exchange的一些函数声明

--- demo_arm/app/auto_exchange.h
+++ demo_arm/app/auto_exchange.h
@@ -4,5 +4,18 @@
 
 #ifndef RM_FRAME_AUTO_EXCHANGE_H
 #define RM_FRAME_AUTO_EXCHANGE_H
+#include "base/robotics/robotics.h"
 
+namespace auto_exchange{
+//视觉信息转换成转移矩阵 cv->T
+Matrixf<4,4> cv2t(void);
+
+//通过视觉信息控制机械臂跟随
+void auto_follow();
+
+}
+
+
+
+//
 #endif //RM_FRAME_AUTO_EXCHANGE_H
